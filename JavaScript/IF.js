d37f91595c2f7019b233f8ea6dcf52547dab7c6f
Arien Aliz <arien@iMac-de-Arien.local>
2022-03-22 12:50:08 -0500

    APUNTES 2

---
+++ JavaScript/IF.js
@@ -0,0 +1,38 @@
+
+/* if (condición) {
+   sentencia1
+} else {
+   sentencia2
+} */
+
+/* Ejemplos:
+if (edad < 18) { 
+    console.log ('No puedes tomar');
+               } else { 
+    console.log ('puedes tomar'); }
+___________________________________________________________
+if ( edad > 50 ) { 
+    console.log ('Adulto mayor'); 
+} else if ( edad > 30) { 
+    console.log ('Adulto') 
+} else { 
+    console.log ('Joven')
+}
+*/
+
+
+// Saber si un número es par
+
+if (num % 2 === 0) {
+    return true
+} else {
+    return false
+}
+
+// Saber si un número es impar
+
+if (num % 2 === 1) {
+    return true
+} else {
+    return false
+}
